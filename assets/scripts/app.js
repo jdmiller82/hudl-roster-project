39b530830fc14e6b6dd2cf3fd7ee067892015bfa
Jonathan Miller <jonathan@miller-interactive.com>
2015-03-27 21:15:09 -0500

    lots and lots of work

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
-  
+  //materializecss init for select dropdowns
   $('select').material_select()
+  
+  //init circliful plugin
+  $('.circle-graph').circliful()
 });
