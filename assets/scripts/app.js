1109515c6ac702b6f505490a3220b66bf6961fd5
Jonathan Miller <jonathan@miller-interactive.com>
2015-03-31 13:16:03 -0500

    adding a few extra niceties...

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   //materializecss init for select dropdowns
   $('select').material_select()
   
+  //materializecss init for modals
+  $('.modal-trigger').leanModal()
+  
   //init circliful plugin
   $('.circle-graph').circliful()
+  
 });
